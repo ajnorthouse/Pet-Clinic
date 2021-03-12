82988a96c936b2bc41d06040a1c1c5dbd79e5f66
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-03-12 14:17:29 -0500

    add missing repo implementations

--- Customer Service/src/main/java/com/cognixia/jump/springcloud/repository/CustomerRespository.java
+++ Customer Service/src/main/java/com/cognixia/jump/springcloud/repository/CustomerRespository.java
@@ -1,5 +1,8 @@
 package com.cognixia.jump.springcloud.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +11,11 @@ import com.cognixia.jump.springcloud.model.Customer;
 @Repository
 public interface CustomerRespository extends JpaRepository<Customer, Integer>{
 	
-	Customer findByCustomerId(Integer customerId);
-
+	List<Customer> findAll();
+	Optional<Customer> findByCustomerId(Integer customerId);
+	Optional<Customer> findByName(String name);
+	Optional<Customer> findByEmailAddress(String email);
+	Optional<Customer> findByPhoneNumber(String phone);
+	Optional<Customer> findByCity(String city);
+	
 }
