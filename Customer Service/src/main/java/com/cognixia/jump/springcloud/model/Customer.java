2941be82a3ae55b52af932cd7cbce0037fbc656b
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-03-12 12:27:25 -0500

    Recode to meet project requirements

--- Customer Service/src/main/java/com/cognixia/jump/springcloud/model/Customer.java
+++ Customer Service/src/main/java/com/cognixia/jump/springcloud/model/Customer.java
@@ -1,6 +1,7 @@
 package com.cognixia.jump.springcloud.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -10,86 +11,107 @@ import javax.persistence.Transient;
 
 @Entity
 public class Customer implements Serializable {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	//class variables
 	@Id
 	@Column(name="CUSTOMERID")
-	Integer customerId;
-	
+	Integer id;
 	@Column(name="NAME")
 	String name;
-	
+	@Column(name="PHONENUMBER")
+	String phoneNumber;
 	@Column(name="EMAILADDRESS")
 	String emailAddress;
+	@Column(name="CITY")
+	String city;
 	
-	@Column(name="ZIPCODE")
-	String Zipcode;
-	
+	//foreign entity
 	@Transient
-	List<Pet> accts;
+	List<Pet> pets;
 	
+	
+	
+	//constructors
 	public Customer() {
-		super();
+		this.id = 0;
+		this.name = "N/A";
+		this.phoneNumber = "N/A";
+		this.emailAddress = "N/A";
+		this.city = "N/A";
+		this.pets = new ArrayList<Pet>();
 	}
-
-	public Customer(Integer customerId, String name, String emailAddress, String zipcode) {
+	public Customer(Integer id, String name, String phoneNumber, String emailAddress, String city) {
 		super();
-		this.customerId = customerId;
+		this.id = id;
 		this.name = name;
+		this.phoneNumber = phoneNumber;
 		this.emailAddress = emailAddress;
-		Zipcode = zipcode;
+		this.city = city;
+		this.pets = new ArrayList<Pet>();
+	}
+	public Customer(Integer id, String name, String phoneNumber, String emailAddress, String city, List<Pet> pets) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.emailAddress = emailAddress;
+		this.city = city;
+		this.pets = pets;
 	}
 
-	public Integer getCustomerId() {
-		return customerId;
+	
+	//getters
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
-
-	public void setCustomerId(Integer customerId) {
-		this.customerId = customerId;
+	public Integer getId() {
+		return id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public String getPhoneNumber() {
+		return phoneNumber;
 	}
-
 	public String getEmailAddress() {
 		return emailAddress;
 	}
+	public String getCity() {
+		return city;
+	}
+	public List<Pet> getPets() {
+		return pets;
+	}
 
+	
+	//setters
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
-
-	public String getZipcode() {
-		return Zipcode;
+	public void setCity(String city) {
+		this.city = city;
 	}
-
-	public void setZipcode(String zipcode) {
-		Zipcode = zipcode;
+	public void setPets(List<Pet> pets) {
+		this.pets = pets;
 	}
-
-	public List<Pet> getAccts() {
-		return accts;
-	}
-
-	public void setAccts(List<Pet> accts) {
-		this.accts = accts;
-	}
-
+	
+	
+	//toString
 	@Override
 	public String toString() {
-		return "Customer [customerId=" + customerId + ", name=" + name + ", emailAddress=" + emailAddress + ", Zipcode="
-				+ Zipcode + "]";
+		return "Customer [id=" + id + ", name=" + name + ", phoneNumber=" + phoneNumber + ", emailAddress="
+				+ emailAddress + ", city=" + city + ", pets=" + pets + "]";
 	}
-
 
 	
 
